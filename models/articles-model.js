583bca9526666548f219c611bd581f0bacc96fed
Jack Harbon <jacek@harbon.uk>
2022-12-12 10:34:46 +0500

    Comments sorted descending

--- models/articles-model.js
+++ models/articles-model.js
@@ -48,7 +48,7 @@ exports.selectArticleById = (article_id) => {
 		});
 };
 exports.selectCommentsByArticleId = (article_id) => {
-	return db.query('SELECT * FROM articles WHERE article_id = $1 ORDER BY comments.created_at DESC;', [article_id]).then((comments) => {
+	return db.query('SELECT * FROM articles WHERE article_id = $1;', [article_id]).then((comments) => {
 		if (comments.rows.length === 0) {
 			return Promise.reject({ status: 404, msg: 'Article Id not found!' });
 		}
